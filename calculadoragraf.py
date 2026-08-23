59346d0b3f07350f048bde67fc1042a0f7e4b92e
Alexanders030 <167349224+Alexanders030@users.noreply.github.com>
2026-08-22 19:33:20 -0500

    sdaf

--- calculadoragraf.py
+++ calculadoragraf.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 from suma import abrir_suma
 # from resta import abrir_resta
-from multiplicacion import abrir_multiplicacion
+# from multiplicacion import abrir_multiplicacion
 # from division import abrir_division
 # from potencia import abrir_potencia
 # from raiz import abrir_raiz
@@ -69,7 +69,7 @@ boton_multiplicacion = tk.Button(
     width=20,
     height=2,
     font=("Arial", 12),
-    command=lambda:abrir_multiplicacion(ventana)  # Temporal: abrir_multiplicacion(ventana)
+    command=lambda: print("Botón MULTIPLICACIÓN presionado")  # Temporal: abrir_multiplicacion(ventana)
 )
 
 boton_multiplicacion.pack(pady=5)
